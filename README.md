18f0dfda6a659155d0f6e13f86eefae333b53356
FenrisStargazer <80987915+FenrisStargazer@users.noreply.github.com>
2022-04-06 13:40:58 -0700

    initial setup
    transfer from codepen

---
+++ README.md
@@ -0,0 +1,12 @@
+# Alice in Wonderland Google Doodle
+
+My topic was “Alice in Wonderland,” but I also wanted to have the classic google design where it spells out “G O O G L E” in the features rather than just written over the top of a scene. So I decided to use some characters from Lewis Carroll’s classic story about Alice in Wonderland to spell out google over the top of a Wonderland-themed backdrop.
+I accepted that my digital art skills are rather awful and need a lot of work, so apologies in advance for the characters looking awful... Also, it's hard to make small SVGs with Figma...
+
+## Collage
+
+https://www.figma.com/file/d0aTHS0k6eI2rGwvG8V1la/My-Art-Direction?node-id=10%3A2
+
+## Resources
+
+ GSAP JavaScript library
---
+++ js/scripts.js
@@ -0,0 +1,317 @@
+console.log('OK');
+
+var JabberwockyTime = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+JabberwockyTime.to('#jabberwock-Z', { 
+  x: -3, 
+  y: -6
+  });
+JabberwockyTime.to('#jabberwock-Z', { 
+  x: 2, 
+  y: -10
+  });
+JabberwockyTime.to('#jabberwock-Z', { 
+  x: -3, 
+  y: -14
+  });
+JabberwockyTime.to('#jabberwock-Z', { 
+  x: 2, 
+  y: -18
+  });
+JabberwockyTime.to('#jabberwock-Z', { 
+  x: -3, 
+  y: -20
+  });
+var JabberwockyTime2 = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+JabberwockyTime2.to('.jabberwockFoot', { 
+  rotation: 8,
+  duration: 2
+  });
+
+var QueenTime = gsap.timeline({repeat: -1, repeatDelay: 3});
+QueenTime.to('#chatQueen', { 
+  opacity: 1,
+  duration: 1,
+  delay: .25
+  });
+QueenTime.to('#chatQueen', { 
+  x: -1,
+  duration: .1
+  });
+QueenTime.to('#chatQueen', { 
+  x: 1,
+  duration: .1
+  });
+QueenTime.to('#chatQueen', { 
+  x: -1,
+  duration: .1
+  });
+QueenTime.to('#chatQueen', { 
+  x: 1,
+  duration: .1
+  });
+QueenTime.to('#chatQueen', { 
+  x: -1,
+  duration: .1
+  });
+QueenTime.to('#chatQueen', { 
+  x: 1,
+  duration: .1
+  });
+QueenTime.to('#chatQueen', { 
+  x: -1,
+  duration: .1
+  });
+QueenTime.to('#chatQueen', { 
+  x: 1,
+  duration: .1
+  });
+QueenTime.to('#chatQueen', { 
+  x: -1,
+  duration: .1
+  });
+QueenTime.to('#chatQueen', { 
+  x: 1,
+  duration: .1
+  });
+QueenTime.to('#chatQueen', { 
+  x: 0,
+  duration: .1
+  });
+QueenTime.to('#chatQueen', {
+  delay: .15,
+  opacity: 0,
+  duration: 1
+  });
+
+var AliceTime = gsap.timeline({repeat: -1, repeatDelay: 3});
+AliceTime.to('.chatAlice', { 
+  opacity: 1,
+  duration: 2
+  });
+AliceTime.to('#chatAlice2', { 
+  transformOrigin:"50% 50%",
+  rotation: 13,
+  duration: 1
+  });
+AliceTime.to('#chatAlice2', { 
+  transformOrigin:"50% 50%",
+  rotation: -13,
+  duration: 1
+  });
+AliceTime.to('#chatAlice2', { 
+  transformOrigin:"50% 50%",
+  rotation: 13,
+  duration: 1
+  });
+AliceTime.to('#chatAlice2', { 
+  transformOrigin:"50% 50%",
+  rotation: -13,
+  duration: 1
+  });
+AliceTime.to('#chatAlice2', { 
+  transformOrigin:"50% 50%",
+  rotation: 0,
+  duration: 1
+  });
+AliceTime.to('.chatAlice', { 
+  opacity: 0,
+  duration: 2
+  });
+
+var RabbitTime = gsap.timeline({repeat: -1, repeatDelay: .25});
+RabbitTime.to('#WR1', { 
+  opacity: 1,
+  duration: .25
+  });
+RabbitTime.to('#WR1', { 
+  opacity: 0,
+  duration: .25
+  });
+var RabbitTime2 = gsap.timeline({repeat: -1, repeatDelay: 3});
+RabbitTime2.to('#chatWR', { 
+  opacity: 1,
+  duration: .75
+  });
+RabbitTime2.to('#chatWR', { 
+  transformOrigin:"50% 50%",
+  rotation: 15,
+  duration: .25
+  });
+RabbitTime2.to('#chatWR', { 
+  transformOrigin:"50% 50%",
+  rotation: -15,
+  duration: .25
+  });
+RabbitTime2.to('#chatWR', { 
+  transformOrigin:"50% 50%",
+  rotation: 15,
+  duration: .25
+  });
+RabbitTime2.to('#chatWR', { 
+  transformOrigin:"50% 50%",
+  rotation: -15,
+  duration: .25
+  });
+RabbitTime2.to('#chatWR', { 
+  transformOrigin:"50% 50%",
+  rotation: 15,
+  duration: .25
+  });
+RabbitTime2.to('#chatWR', { 
+  transformOrigin:"50% 50%",
+  rotation: -15,
+  duration: .25
+  });
+RabbitTime2.to('#chatWR', { 
+  transformOrigin:"50% 50%",
+  rotation: 0,
+  duration: .1
+  });
+RabbitTime2.to('#chatWR', { 
+  opacity: 0,
+  duration: .75
+  });
+var RabbitTime3 = gsap.timeline({repeat: -1, repeatDelay: .25});
+RabbitTime3.to('#foot1', { 
+  fill: "rgb(77, 77, 77)",
+  duration: .5
+  });
+RabbitTime3.to('#foot1', { 
+  fill: "rgb(191, 191, 191)",
+  duration: .25
+  });
+RabbitTime3.to('#foot2', { 
+  fill: "rgb(77, 77, 77)",
+  duration: .5
+  });
+RabbitTime3.to('#foot2', { 
+  fill: "rgb(191, 191, 191)",
+  duration: .25
+  });
+RabbitTime3.to('#foot3', { 
+  fill: "rgb(77, 77, 77)",
+  duration: .5
+  });
+RabbitTime3.to('#foot3', { 
+  fill: "rgb(191, 191, 191)",
+  duration: .25
+  });
+RabbitTime3.to('#foot4', { 
+  fill: "rgb(77, 77, 77)",
+  duration: .5
+  });
+RabbitTime3.to('#foot4', { 
+  fill: "rgb(191, 191, 191)",
+  duration: .25
+  });
+RabbitTime3.to('#foot5', { 
+  fill: "rgb(77, 77, 77)",
+  duration: .5
+  });
+RabbitTime3.to('#foot5', { 
+  fill: "rgb(191, 191, 191)",
+  duration: .25
+  });
+RabbitTime3.to('#foot6', { 
+  fill: "rgb(77, 77, 77)",
+  duration: .5
+  });
+RabbitTime3.to('#foot6', { 
+  fill: "rgb(191, 191, 191)",
+  duration: .25
+  });
+RabbitTime3.to('#foot7', { 
+  fill: "rgb(77, 77, 77)",
+  duration: .5
+  });
+RabbitTime3.to('#foot7', { 
+  fill: "rgb(191, 191, 191)",
+  duration: .25
+  });
+RabbitTime3.to('#foot8', { 
+  fill: "rgb(77, 77, 77)",
+  duration: .5
+  });
+RabbitTime3.to('#foot8', { 
+  fill: "rgb(191, 191, 191)",
+  duration: .25
+  });
+RabbitTime3.to('#foot9', { 
+  fill: "rgb(77, 77, 77)",
+  duration: .5
+  });
+RabbitTime3.to('#foot9', { 
+  fill: "rgb(191, 191, 191)",
+  duration: .25
+  });
+RabbitTime3.to('#foot10', { 
+  fill: "rgb(77, 77, 77)",
+  duration: .5
+  });
+RabbitTime3.to('#foot10', { 
+  fill: "rgb(191, 191, 191)",
+  duration: .25
+  });
+RabbitTime3.to('#foot11', { 
+  fill: "rgb(77, 77, 77)",
+  duration: .5
+  });
+RabbitTime3.to('#foot11', { 
+  fill: "rgb(191, 191, 191)",
+  duration: .25
+  });
+RabbitTime3.to('#foot12', { 
+  fill: "rgb(77, 77, 77)",
+  duration: .5
+  });
+RabbitTime3.to('#foot12', { 
+  fill: "rgb(191, 191, 191)",
+  duration: .25
+  });
+
+var CaterTime = gsap.timeline({repeat: -1, repeatDelay: 1, yoyo: true});
+CaterTime.to('.bubbles', { 
+  x: 1,
+  y: -1
+  });
+CaterTime.to('.bubbles', { 
+  x: 0,
+  y: -2
+  });
+CaterTime.to('.bubbles', { 
+  x: 1,
+  y: -3
+  });
+
+var CatTime = gsap.timeline({repeat: -1, repeatDelay: 0});
+CatTime.to('#chatCat', {
+  y: -5
+  });
+CatTime.to('#chatCat', {
+  y: 0
+  });
+var CatTime2 = gsap.timeline({repeat: -1, repeatDelay: 0});
+CatTime2.to('#chatCat', { 
+  opacity: 0,
+  duration: 1
+  });
+CatTime2.to('#chatCat', { 
+  opacity: 1,
+  duration: 1
+  });
+CatTime2.to('#chatCat', { 
+  opacity: 0,
+  duration: 1
+  });
+CatTime2.to('#chatCat', { 
+  opacity: 1,
+  duration: 1
+  });
+CatTime2.to('#chatCat', { 
+  opacity: 0,
+  duration: 1
+  });
+CatTime2.to('#chatCat', { 
+  opacity: 1,
+  duration: 1
+  });
